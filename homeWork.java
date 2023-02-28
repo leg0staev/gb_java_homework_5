58adbcd712ede72ecd7559ff12ef5fd2afbce217
legostaev <alexey.legostaev@gmail.com>
2023-02-28 16:01:48 +0300

    two poiners method added

--- homeWork.java
+++ homeWork.java
@@ -63,4 +63,40 @@ public class homeWork {
         return maxLength;
     }
 
+    // с помощю двух указателей
+    public static int findLength(int[] nums1, int[] nums2) {
+        int maxLength = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int p1 = i;
+            int p2 = 0;
+            int len = 0;
+            while (p1 < nums1.length && p2 < nums2.length) {
+                if (nums1[p1] == nums2[p2]) {
+                    len++;
+                    maxLength = Math.max(maxLength, len);
+                } else {
+                    len = 0;
+                }
+                p1++;
+                p2++;
+            }
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            int p1 = 0;
+            int p2 = i;
+            int len = 0;
+            while (p1 < nums1.length && p2 < nums2.length) {
+                if (nums1[p1] == nums2[p2]) {
+                    len++;
+                    maxLength = Math.max(maxLength, len);
+                } else {
+                    len = 0;
+                }
+                p1++;
+                p2++;
+            }
+        }
+        return maxLength;
+    }
+
 }
